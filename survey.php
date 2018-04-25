3bdc92e46969491eb6713fe9f11724ea51d9d4e6
Charlotte Wright - Spectre <RandomWright@users.noreply.github.com>
2018-04-24 21:31:15 -0400

    survey

---
+++ survey.php
@@ -0,0 +1,41 @@
+<?php
+
+$page = "Survey";
+
+include "includes/html-top.php";
+
+?>
+
+	<body>
+		<?php
+
+			include "includes/nav.php";
+			include "includes/header.php";
+
+		?>
+
+			<h1>Survey</h1>
+
+			<hr>
+
+			<div class="logo">
+				<img src="images/apple.png" alt="Apple logo">
+			</div>
+
+			<div class="logo">
+				<img src="images/google.png" alt="Google logo">
+			</div>
+
+			<div class="logo">
+				<img src="images/microsoft.png" alt="Microsoft logo">
+			</div>
+
+			<hr>
+
+			</div>
+
+			<?php include "includes/footer.php"; ?>
+
+	</body>
+
+	</html>
