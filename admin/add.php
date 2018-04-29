c53f6caac7d6dcc5963bfa7f4edfcdc05bd6d5ce
Charlotte Wright - Spectre <RandomWright@users.noreply.github.com>
2018-04-28 23:24:28 -0400

    small fix

--- admin/add.php
+++ admin/add.php
@@ -4,7 +4,7 @@
  $dbpass = "EssLikeNeme";
  $dbname = "urcscon3_newyork5";
  $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname); 
-$sql = "INSERT INTO survey(name, brand, computer) VALUES('".$_POST["name"]."', '".$_POST["last_name"]."', '".$_POST["computer"]."')";  
+$sql = "INSERT INTO survey(name, brand, computer) VALUES('".$_POST["name"]."', '".$_POST["brand"]."', '".$_POST["computer"]."')";  
 if(mysqli_query($connection, $sql))  
 {  
      echo 'Data Inserted';  
