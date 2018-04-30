075d3400dedd3f4caacdad8145d5311f2cf2cf54
LudwigNova <lnova@u.rochester.edu>
2018-04-30 03:22:57 -0400

    Final Design Changes!

--- login.php
+++ login.php
@@ -4,48 +4,48 @@
  $dbuser = "urcscon3_cwright";
  $dbpass = "EssLikeNeme";
  $dbname = "urcscon3_newyork5";
- $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname); 
- 
+ $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
 // Define variables and initialize with empty values
 $username = $password = "";
 $username_err = $password_err = "";
- 
+
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
+
     // Check if username is empty
     if(empty(trim($_POST["username"]))){
         $username_err = 'Please enter username.';
     } else{
         $username = trim($_POST["username"]);
     }
-    
+
     // Check if password is empty
     if(empty(trim($_POST['password']))){
         $password_err = 'Please enter your password.';
     } else{
         $password = trim($_POST['password']);
     }
-    
+
     // Validate credentials
     if(empty($username_err) && empty($password_err)){
         // Prepare a select statement
         $sql = "SELECT username, password FROM users WHERE username = ?";
-        
+
         if($stmt = mysqli_prepare($connection, $sql)){
             // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "s", $param_username);
-            
+
             // Set parameters
             $param_username = $username;
-            
+
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
                 // Store result
                 mysqli_stmt_store_result($stmt);
-                
+
                 // Check if username exists, if yes then verify password
-                if(mysqli_stmt_num_rows($stmt) == 1){                    
+                if(mysqli_stmt_num_rows($stmt) == 1){
                     // Bind result variables
                     mysqli_stmt_bind_result($stmt, $username, $hashed_password);
                     if(mysqli_stmt_fetch($stmt)){
@@ -53,7 +53,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             /* Password is correct, so start a new session and
                             save the username to the session */
                             session_start();
-                            $_SESSION['username'] = $username;      
+                            $_SESSION['username'] = $username;
                             header("location: admin.php");
                         } else{
                             // Display an error message if password is not valid
@@ -68,11 +68,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 echo "Oops! Something went wrong. Please try again later.";
             }
         }
-        
+
         // Close statement
         mysqli_stmt_close($stmt);
     }
-    
+
     // Close connection
     mysqli_close($connection);
 }
@@ -94,29 +94,33 @@ include "includes/html-top.php";
 			include "includes/header.php";
 
 		?>
-		
+
 		<div class="wrapper">
         <h2>Login</h2>
         <p>Please fill in your credentials to login.</p>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
                 <label>Username</label>
+                <br/>
                 <input type="text" name="username"class="form-control" value="<?php echo $username; ?>">
                 <span class="help-block"><?php echo $username_err; ?></span>
-            </div>    
+            </div>
             <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <br/>
                 <label>Password</label>
+                <br/>
                 <input type="password" name="password" class="form-control">
                 <span class="help-block"><?php echo $password_err; ?></span>
             </div>
             <div class="form-group">
+                <br/>
                 <input type="submit" class="btn btn-primary" value="Login">
             </div>
             <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
         </form>
-		</div>    
+		</div>
 
-			
+
 
 
 		<?php include "includes/footer.php"; ?>
--- survey.php
+++ survey.php
@@ -20,21 +20,31 @@ include "includes/html-top.php";
 			<h1>Survey</h1>
 
 
-			<form action="insert.php" method="post">
+			<form class="surveyForm" action="insert.php" method="post">
 			<fieldset>
 			<label for="name">Name</label>
+			<br/>
 			<input type="text" id="name" name="name" placeholder="Your name..">
 
+			<br/>
+			<br/>
 			<label for="country">Pick One</label>
+
+			<br/>
 			<select id="brand" name="brand">
 				<option value="apple">Apple</option>
 				<option value="mircosoft">Mircosoft</option>
 				<option value="google">Google</option>
 			</select>
 
+			<br/>
+			<br/>
 			<label for="computer">What type of computer do you use?</label>
+			<br/>
 			<input type="text" id="computer" name="computer">
 
+			<br/>
+			<br/>
 			<input type="submit" value="Submit">
 			</fieldset>
 			</form>
